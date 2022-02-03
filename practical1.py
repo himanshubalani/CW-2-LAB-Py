5abf3910ddafabb31752790d08e991c7d824fb73
Himanshu Balani <85930567+himanshubalani@users.noreply.github.com>
2022-02-03 16:12:36 +0530

    Added practical1

---
+++ practical1.py
@@ -0,0 +1,8 @@
+a = int(input('Enter your first number: '))
+b = int(input('Enter your second number: '))
+print("Addition: ",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Float division:",a/b)
+print("Integer division:",a//b)
+print("Quotient:",a%b)
